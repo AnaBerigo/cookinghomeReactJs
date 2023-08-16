9c70bf02149b9752b12af620a8fc5fa66646fbd1
Ana Paula Berigo <ana.paula@sstelematica.com.br>
2023-08-16 09:01:24 -0300

    first commit

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components"
+
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        width:100vw;
+        height:100vh;
+        font-family: 'Quicksand', sans-serif;
+    }
+`;
+
+export default GlobalStyles;
